44df5fb55205b348475ade5fc1427ecc27155a2b
xyzroller <xyzroller@rollyourown.xyz>
2022-04-24 19:16:10 +0200

    Bump consul-template version to 0.28.1

--- scripts-module/build-images.sh
+++ scripts-module/build-images.sh
@@ -5,7 +5,7 @@
 
 # Default software versions
 webhook_version='2.8.0'
-consul_template_version='0.27.2'
+consul_template_version='0.28.1'
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
